a1196787befc25b480accf5315974ace8d2b9652
https://github.com/Wasi752 <osiur2@gmail.com>
2023-02-26 22:26:40 +0600

    Drop Down

--- src/Home/stockCategory.js
+++ src/Home/stockCategory.js
@@ -1,12 +1,21 @@
 import HeaderANavbar from "./HeaderANavbar";
+import { Link } from "react-router-dom";
 
-function BookCategory(){
-  return(
+function BookCategory() {
+  return (
     <div className="w-full h-full">
-      <HeaderANavbar/>
-      <div>Amar Nam</div>
+      <HeaderANavbar />
+      <div className="flex w-full h-full mt-10">
+        <div className="w-[20%] h-full bg-green-100 mt-10">
+          <Link to={"/sdetails/1"}><p className="text-2xl font-serif font-extrabold ml-10 mb-5 mt-40 text-blue-500">Staff Details</p></Link>
+          <Link to="/summary/staff"><p className="text-2xl font-serif font-extrabold ml-10 mb-5 mt-5 text-blue-500">Create New Staff Data</p></Link>
+        </div>
+        <div className="flex flex-col ml-5 w-[60%] h-[80%] mt-10 bg-gray-100">
+          <p className="text-2xl font-serif font-extrabold ml-10 mb-5 mt-10 text-green-800">Staff Summary</p>
+        </div>
+      </div>
     </div>
-  )
+      )
 }
- 
- export default BookCategory;
+
+export default BookCategory;
