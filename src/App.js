eed4e576a912cfe399614f91ef3ef32ca50e24fd
wasi752 <osiur2@gmail.com>
2023-03-02 15:45:06 +0600

    Try AAA

--- src/App.js
+++ src/App.js
@@ -1,32 +1,20 @@
-import './App.css';
-import { Routes, Route } from "react-router-dom";
-import Home from './FirebasePages/Home';
-import SignUp2 from './FirebasePages/Signup';
-import Login from './FirebasePages/Login';
-{/* 
-import SignUp from './Pages/signUp';
-import Signin from './Pages/signIn';
-import ChangePassword from './Pages/changePassword';
-import RecoverPassword from './Pages/recoverPassword';
-import BookCategory from './Pages/Dashboard';
-import { AuthProvider } from './context/AuthContext'; 
-*/}
-
+import "./App.css";
+//import { Routes, Route } from "react-router-dom";
+import Home from "./FirebasePages/HomeFirebase";
+import SignUp2 from "./FirebasePages/SignupFirebase";
+import Login from "./FirebasePages/LoginFirebase";
+import { AuthProvider } from "./context/AuthContext";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
       <Routes>
-        {/*<AuthProvider>
-            <Route path="/" element={<Signin/>} />
-        <Route path="/signup" element={<SignUp/>} />
-        <Route path="forget" element={<RecoverPassword/>} />
-        <Route path="/change/:id" element={<ChangePassword/>} />
-  <Route path="/home/:id" element={<BookCategory/>} /> 
-        </AuthProvider> */}
-        <Route path="/" element={<Home />} />
-        <Route path="/signup2" element={<SignUp2 />} />
-        <Route path="/login" element={<Login />} />
+        {/* <AuthProvider> */}
+          <Route path="/" element={<Home />} />
+          <Route path="/signup2" element={<SignUp2 />} />
+          <Route path="/login" element={<Login />} />
+        {/* </AuthProvider> */}
       </Routes>
     </div>
   );
